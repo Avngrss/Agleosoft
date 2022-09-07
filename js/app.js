c08c63b54dc3779eac027a03ce1dccf6da214fa6
Avngrss <97980284+Avngrss@users.noreply.github.com>
2022-09-07 18:49:44 +0300

    add: add header, modal, mobile menu and one sectio

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//modal
+const btn = document.querySelector("#btn");
+const wrap = document.querySelector(".wrapper__modal");
+const btnClose = document.querySelector(".btn__close");
+const overlay = document.querySelector(".overlay");
+
+btn.addEventListener("click", () => {
+  wrap.classList.add("active");
+});
+
+const closeModal = () => {
+  wrap.classList.remove("active");
+};
+overlay.addEventListener("click", closeModal);
+btnClose.addEventListener("click", closeModal);
+
+//Mobile menu
+const menuBtn = document.querySelector(".menu__btn-mobile");
+const menu = document.querySelector(".menu__item");
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+});
